944463be0dcd3544d03f99eed00c749f5018a1dd
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-04-27 00:36:12 -0300

    Commit inicial com prova de conceito
    Inclui o script para criar os links no HOME e o .conkyrc

---
+++ create_symlinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+RELDIR=$(dirname $0)
+CURDIR=`cd $RELDIR && pwd` #workaround to print the entire path
+
+link() {
+    # Creates a symbolic link to the file you pass in your home
+    # If you have a file in a subdirectory, you should mirror
+    # that structure here. This is on purpose
+    FILEPATH=$1
+    TARGET="$CURDIR/$FILEPATH"
+    LINKNAME="$HOME/$FILEPATH"
+    ln -is $TARGET $LINKNAME
+}
+
+link .conkyrc
