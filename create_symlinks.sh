7f922c7a026fffcceb0c2b61722985e2cd3270c5
Flavio Amieiro <amieiro.flavio@gmail.com>
2011-01-12 00:55:16 -0200

    Adiciona a possibilidade de criar diretórios se eles não existirem

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -12,6 +12,29 @@ link() {
     ln -is $TARGET $LINKNAME
 }
 
+create_dir_if_it_does_not_exist() {
+    # Checks for the existence of a directory named "$1". If $1
+    # doesn't exist asks the user if it should be created.
+    #
+    # Returns 1 without creating the directory if the user answers no.
+    # Returns 0 after creating the directory if the user answers yes
+    # or if the directory already existed.
+    DIR="$1"
+    if [ ! -d "$DIR" ]
+    then
+	read -p "$DIR not found. create it? [Y/n] " ANSWER
+	# FIXME: There should be a better way to do this
+	if [ $ANSWER = "y" -o $ANSWER = "Y" ]
+	then
+	    echo "Creating $DIR ..."
+	    mkdir -p "$DIR"
+	else
+	    return 1
+	fi
+    fi
+    return 0
+}
+
 link .conkyrc
 link .bashrc
 link .coderc
@@ -30,7 +53,8 @@ link .Xdefaults
 link .Xmodmap
 link .xinitrc
 
-if [ -d $HOME/.config/openbox/ ]
+create_dir_if_it_does_not_exist "$HOME/.config/openbox/"
+if [ $? -eq 0 ]
 then
     link .config/openbox/autostart.sh
     link .config/openbox/menu.xml
@@ -39,7 +63,8 @@ else
     echo "$HOME/.config/openbox/ not found!"
 fi
 
-if [ -d $HOME/bin/ ]
+create_dir_if_it_does_not_exist "$HOME/bin/"
+if [ $? -eq 0 ]
 then
     link bin/volume_control
 else
